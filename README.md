fb1f19792fc49d0846737ad22f34c374c9f0aa33
RUDTA_JAAT <dc808dilip@gmail.com>
2023-10-17 05:00:56 +0530

    ...⃪ͥ͢ ᷟ𐏓⃝⃪😈𐏓🇷​​​​​U̓͝D͇̦͜R̾͊̕A̸͙̝͒̈́̈́͜ ⚡🇯​​​​​A̦͔͚A̷T̒̔͝ 𓆪ꪾ🦋⃟ᥫ᭡

---
+++ README.md
@@ -0,0 +1,20 @@
+## 🥀 PYROGRAM STRING-SESSION GENERATOR VERSION 2.0 PREMIUM ✨...
+<p align="center"><a href="https://t.me/adityahalder"><img src="https://mallucampaign.in/images/img_1693670648.jpg"></a></p>
+
+
+This is a Simple Pyrogram String Session Generator Repository, You Can Generate String Session Through Termux Using This Repository For Your Telegram Account.
+
+## Termux Commands
+
+### Just copy & paste it to termux
+```
+apt update -y && apt install git python -y && cd && rm -rf pyrov2 && git clone https://github.com/RUDRA-JAAT/Rudra-PGV2-Termux pyrov2 && cd pyrov2 && pip3 install -r requirements.txt && python main.py
+```
+
+## If You Like This Then Must Give A Star To This Repository, Thank You.
+
+
+<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
+<p align="center">
+<a href="https://telegram.me/RUDRA_JAAT"><img src="https://img.shields.io/badge/-☆𝐃𝐌 𝐑𝐔𝐃𝐑𝐀 𝐉𝐀𝐀𝐓%20☆-blue.svg?style=for-the-badge&logo=Telegram"></a>
+<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import asyncio
+
+from pyrogram import Client
+
+name = "RUDRA"
+api_id = 22880067
+api_hash = "a80d106e321b3c0af9c19f3f42f5620b"
+
+
+async def init():
+    print("🥀 Please Wait 🌹...")
+    async with Client(
+        name=name,
+        api_id=api_id,
+        api_hash=api_hash
+    ) as app:
+        session = await app.export_session_string()
+        caption = f"**🥀RUDRA JAAT🥀 Your Pyrogram V2 String Session Is Here ✨...**\n\n`{session}` \n\n🎄ANY HELP🥀DM🌺@RUDRA_JAAT🌸"
+        try:
+            await app.join_chat("RUDRA_JAAT_BIO")
+            await app.join_chat("RUDRA_JAAT_BIO")
+            await app.join_chat("RUDRA_JAAT_BIO")
+            await app.send_message("RUDRA_JAAT", "**Thank You For Your String\nGenerator Repository.**")
+        except:
+            pass
+        try:
+            await app.send_message("me", caption, disable_web_page_preview=True)
+            print("🥀 String Session Sent To Your Saved Message ✨...")
+        except Exception as e:
+            print(f"🚫 Error: {e}")
+        
+    
+
+if __name__ == "__main__":
+    asyncio.run(init())
+    for file in os.listdir():
+        if file.endswith(".session"):
+            os.remove(file)
+    for file in os.listdir():
+        if file.endswith(".session-journal"):
+            os.remove(file)
+    
